63a22da462f88be9b68e5c9cab0beb6093bf3ad9
Ignas Paplauskas <ignas.paplauskas@gmail.com>
2015-08-15 15:24:15 +0300

    block, field features

---
+++ coloringScript.js
@@ -0,0 +1,8 @@
+
+    var color = document.getElementsByClassName("e").style.backgroundColor;
+    if (color == "black")
+        document.getElementsByClassName("e").style.backgroundColor="red";
+    console.log("it works!");
+
+
+console.log('hello');
