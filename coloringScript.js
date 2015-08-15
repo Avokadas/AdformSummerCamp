ea41d4762050a8412b822f0f6749adfdb1be9ef9
Ignas Paplauskas <ignas.paplauskas@gmail.com>
2015-08-15 23:52:42 +0300

    logic update

--- coloringScript.js
+++ coloringScript.js
@@ -1,49 +1,125 @@
-function shapeColoring(type) {
-    var shape = document.getElementsByClassName(type);
+var lineLength = 10;
 
-    for (var i = 0; i < shape.length; i++) {
-        shape[i].style.backgroundColor = "red";
-    }
+function changeDivClass(x) {
+    //d3.select(d3.select(".tetris").selectAll("div")[0][x]).attr("class", "colored");
+    d3.select(d3.selectAll(".spawn")[0][x]).attr("style", "background-color:red");
+
 }
-
-function bringBlocksDownByOne() {
-    var rows = document.getElementsByClassName("row");
-    for (var i = rows.length - 1; i >= 0; i--) {
-        for (j = 0; j < rows[i].childElementCount; j++) {
-            if (i > 0) {
-                if (rows[i - 1].children[j].style.backgroundColor == "red") {
-                    rows[i].children[j].style.backgroundColor = "red";
-                    rows[i - 1].children[j].style.backgroundColor = "black";
-                }
+function repaintMatrix(matrixx) {
+    for (i = 0; i < 8;) {
+        for (j = 0; j < 5;) {
+            if (matrixx[i][j] == 1) {
+                //changeclass
+                changeDivClass(i * 5 + j);
             }
+            j += 1;
         }
+        i += 1;
     }
 }
 
-var shapetype = ['i', 'o', 't', 's', 'z', 'j', 'l'];
+function add3Bot() {
+    changeDivClass(Math.floor(lineLength / 2) + lineLength);
+    changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength);
+    changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength);
+}
 
-var speed = 1000;
+var shapeArr = [
+    function () {
+        add3Bot();
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength + 1);
+    },
+    function () {
+        add3Bot();
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength - 1);
+    },
+    function () {
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength);
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength - 1);
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength);
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength - 1);
+    },
+    function () {
+        changeDivClass(Math.floor(lineLength / 2));
+        add3Bot();
+    },
+    function () {
+        add3Bot();
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength - 1);
+    },
+    function () {
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength);
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength + 1);
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength);
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength - 1);
+    },
+
+
+    function () {
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength);
+        changeDivClass(Math.floor(lineLength / 2) + 3 * lineLength - 1);
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength);
+        changeDivClass(Math.floor(lineLength / 2) + 2 * lineLength + 1);
+    }
+];
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    //your code to run since DOM is loaded and ready
-    shapeColoring(shapetype[Math.floor(Math.random() * 6) + 0]);
-
+    var rows = document.getElementsByClassName("row");
+    for (var i = 0; i < rows.length; i++)
+        for (var j = 0; j < rows[i].childElementCount; j++) {
+            rows[i].children[j].style.backgroundColor = "black";
+        }
+    shapeArr[Math.floor(Math.random() * shapeArr.length)]();
 });
 
 
+function bringBlocksDownByOne() {
+    var rows = document.getElementsByClassName("row");
+    var lai = false;
+    for (var i = rows.length - 1; i >= 0; i--) {
+        for (j = 0; j < rows[i].childElementCount; j++) {
+            if (i > 0) {
+                if ((rows[i - 1].children[j].style.backgroundColor == "red" ) && (rows[i].children[j].style.backgroundColor == "black")) {
+                    rows[i].children[j].style.backgroundColor = "red";
+                    rows[i - 1].children[j].style.backgroundColor = "black";
+                    if (i == rows.length - 1) {
+                        lai = true;
+                    }
+                }
+
+            }
+        }
+    }
+    if (lai == true) {
+        for (var i = rows.length - 1; i >= 0; i--)
+            for (j = 0; j < rows[i].childElementCount; j++)
+                if (rows[i].children[j].style.backgroundColor == "red")
+                    rows[i].children[j].style.backgroundColor = "green";
+        lai = false;
+    }
+}
+
+
+var speed = 1000;
 
 
 document.addEventListener('keydown', function (event) {
     if (event.keyCode == 40) {
-        speed=speed*10;
+        speed = speed * 10;
     }
 });
 
-function callback(){
+document.addEventListener('keydup', function (event) {
+    if (event.keyCode == 38) {
+        rotateShape();
+    }
+});
+
+function callback() {
     bringBlocksDownByOne();
-    setTimeout(callback,speed)
+
+    setTimeout(callback, speed)
 }
 
-
-
-setTimeout(callback,speed);
+setTimeout(callback, speed);
