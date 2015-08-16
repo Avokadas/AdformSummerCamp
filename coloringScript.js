8cad387b80eb083692065f2558f35c5af89b5e3b
Ignas Paplauskas <ignas.paplauskas@gmail.com>
2015-08-16 03:50:22 +0300

    rotation implementation

--- coloringScript.js
+++ coloringScript.js
@@ -1,150 +1,138 @@
 var lineLength = 10;
-var shapeState = 0;
+var shapeState;
+var shapeIndex;
 var speed = 400;
-var x=0;
-var y=0;
+var x = 5;
+var y = 0;
 
 
-function startWithCoordinate(shape, row, column) {
+function startWithCoordinate(shape, row, column,attribute) {
     var size = Math.sqrt(shape.length);
     for (i = 0; i < shape.length; i++) {
         if (shape[i] == 1) {
-            changeDivClass((row + Math.floor(i / size)) * lineLength + column + (i % size));
+            changeDivClass((row + Math.floor(i / size)) * lineLength + column + (i % size),attribute);
         }
     }
 }
 
-//FIGURU TEMPLATE IR STATE
-
-shapeT = [
-    [0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 1, 1, 1,
-        0, 0, 1, 0],
-    [0, 0, 0, 0,
-        0, 0, 1, 0,
-        0, 1, 1, 0,
-        0, 0, 1, 0],
-    [0, 0, 0, 0,
-        0, 0, 1, 0,
-        0, 1, 1, 1,
-        0, 0, 0, 0],
-    [0, 0, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 1,
-        0, 0, 1, 0]
-];
-
-shapeJ = [
-    [0, 0, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 1, 1, 0],
-    [0, 0, 0, 0,
-        1, 0, 0, 0,
-        1, 1, 1, 0,
-        0, 0, 0, 0],
-    [0, 1, 1, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 0, 0],
-    [0, 0, 0, 0,
-        0, 1, 1, 1,
-        0, 0, 0, 1,
-        0, 0, 0, 0]
-];
-
-
-shapeL = [
-    [0, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 1, 0],
-    [0, 0, 0, 0,
-        1, 1, 1, 0,
-        1, 0, 0, 0,
-        0, 0, 0, 0],
-    [0, 1, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 0],
-    [0, 0, 0, 0,
-        0, 0, 0, 1,
-        0, 1, 1, 1,
-        0, 0, 0, 0]
-];
-
-shapeO = [
-    [0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 1, 1, 0,
-        0, 1, 1, 0]
-];
-
-shapeI = [
-    [0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0],
-    [0, 0, 0, 0,
-        0, 0, 0, 0,
-        1, 1, 1, 1,
-        0, 0, 0, 0],
-    [0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0],
-    [0, 0, 0, 0,
-        1, 1, 1, 1,
-        0, 0, 0, 0,
-        0, 0, 0, 0]
-];
-
-
-shapeZ = [
-    [0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 1, 1, 0,
-        0, 0, 1, 1],
-    [0, 0, 0, 0,
-        0, 0, 1, 0,
-        0, 1, 1, 0,
-        0, 1, 0, 0]
-];
-
-
-shapeS = [
+shapeArray = [
     [
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 1, 1, 0,
-        1, 1, 0, 0
+        [0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 1, 1, 1,
+            0, 0, 1, 0],
+        [0, 0, 0, 0,
+            0, 0, 1, 0,
+            0, 1, 1, 0,
+            0, 0, 1, 0],
+        [0, 0, 0, 0,
+            0, 0, 1, 0,
+            0, 1, 1, 1,
+            0, 0, 0, 0],
+        [0, 0, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 1,
+            0, 0, 1, 0]
     ],
     [
-        0, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 1, 0,
-        0, 0, 1, 0
+        [0, 0, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 1, 1, 0],
+        [0, 0, 0, 0,
+            1, 0, 0, 0,
+            1, 1, 1, 0,
+            0, 0, 0, 0],
+        [0, 1, 1, 0,
+            0, 1, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 0, 0],
+        [0, 0, 0, 0,
+            0, 1, 1, 1,
+            0, 0, 0, 1,
+            0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 1, 0, 0,
+            0, 1, 1, 0],
+        [0, 0, 0, 0,
+            1, 1, 1, 0,
+            1, 0, 0, 0,
+            0, 0, 0, 0],
+        [0, 1, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 0],
+        [0, 0, 0, 0,
+            0, 0, 0, 1,
+            0, 1, 1, 1,
+            0, 0, 0, 0]
+    ],
+    [
+        [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 1, 1, 0,
+            0, 1, 1, 0]
+    ],
+    [
+        [
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0],
+        [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            1, 1, 1, 1,
+            0, 0, 0, 0],
+        [
+            0, 1, 0, 0,
+            0, 1, 0, 0,
+            0, 1, 0, 0,
+            0, 1, 0, 0],
+        [
+            0, 0, 0, 0,
+            1, 1, 1, 1,
+            0, 0, 0, 0,
+            0, 0, 0, 0]
+    ],
+    [
+        [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 1, 1, 0,
+            0, 0, 1, 1],
+        [
+            0, 0, 0, 0,
+            0, 0, 1, 0,
+            0, 1, 1, 0,
+            0, 1, 0, 0
+        ]
+    ],
+    [
+
+        [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 1, 1, 0,
+            1, 1, 0, 0
+        ],
+        [
+            0, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 1, 1, 0,
+            0, 0, 1, 0
+        ]
+
     ]
 ];
 
 
-function changeDivClass(x) {
-    //d3.select(d3.select(".tetris").selectAll("div")[0][x]).attr("class", "colored");
-    d3.select(d3.selectAll(".e")[0][x]).attr("style", "background-color:red");
-
-}
-function repaintMatrix(matrixx) {
-    for (i = 0; i < 8;) {
-        for (j = 0; j < 5;) {
-            if (matrixx[i][j] == 1) {
-                //changeclass
-                changeDivClass(i * 5 + j);
-            }
-            j += 1;
-        }
-        i += 1;
-    }
+function changeDivClass(x,attribute) {
+    d3.select(d3.selectAll(".e")[0][x]).attr("style", attribute);
 }
 
 
@@ -154,11 +142,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
         for (var j = 0; j < rows[i].childElementCount; j++) {
             rows[i].children[j].style.backgroundColor = "black";
         }
-    //shapeArr[Math.floor(Math.random() * shapeArr.length)]();
-    startWithCoordinate(shapeT[Math.floor(Math.random() * shapeT.length)], x, y);
 
-    var c;
-
+    shapeIndex=Math.floor(Math.random() * shapeArray.length);
+    startWithCoordinate(shapeArray[shapeIndex][0], y, x,"background-color:red");
+    shapeState = 0;
 
     //console.log(x);
 
@@ -166,30 +153,59 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 function bringBlocksDownByOne() {
-
-
+    y += 1;
     var rows = document.getElementsByClassName("row");
     var lai = false;
-    for (var i = rows.length - 1; i >= 0; i--) {
-        for (j = 0; j < rows[i].childElementCount; j++) {
-            if (i > 0) {
-                if ((rows[i - 1].children[j].style.backgroundColor == "red" ) && (rows[i].children[j].style.backgroundColor == "black")) {
-                    rows[i].children[j].style.backgroundColor = "red";
-                    rows[i - 1].children[j].style.backgroundColor = "black";
-                    if (i == rows.length - 1) {
-                        lai = true;
-                    }
+    var temp = 0;
+    for (var i = rows.length - 1; i >= 0; i--)
+        for (j = 0; j < rows[i].childElementCount; j++)
+            if (i > 0)
+                if ((rows[i - 1].children[j].style.backgroundColor == "red") && ((rows[i].children[j].style.backgroundColor == "black") || (rows[i].children[j].style.backgroundColor == "red"))) {
+                    temp++;
+                    //console.log(temp);
                 }
+    if (temp == 4) {
+        for (var i = rows.length - 1; i >= 0; i--) {
+            for (j = 0; j < rows[i].childElementCount; j++) {
+                if (i > 0) {
+                    if ((rows[i - 1].children[j].style.backgroundColor == "red" ) && (rows[i].children[j].style.backgroundColor == "black")) {
+                        rows[i].children[j].style.backgroundColor = "red";
+                        rows[i - 1].children[j].style.backgroundColor = "black";
+                        if (i == rows.length - 1) {
+                            lai = true;
+                        }
+                    }
 
+                }
             }
         }
     }
+    else {
+        for (i = rows.length - 1; i >= 0; i--)
+            for (j = 0; j < rows[i].childElementCount; j++)
+                if (i > 0)
+                    if (rows[i].children[j].style.backgroundColor == "red") {
+                        rows[i].children[j].style.backgroundColor = "green";
+                        x = 0;
+                        y = 0;
+                        shapeIndex=Math.floor(Math.random() * shapeArray.length);
+                        startWithCoordinate(shapeArray[shapeIndex][0], y, x,"background-color:red");
+                        shapeState = 0;
+                    }
+
+    }
+
     if (lai == true) {
         for (i = rows.length - 1; i >= 0; i--)
             for (j = 0; j < rows[i].childElementCount; j++)
                 if (rows[i].children[j].style.backgroundColor == "red")
                     rows[i].children[j].style.backgroundColor = "green";
         lai = false;
+        x = 0;
+        y = 0;
+        shapeIndex=Math.floor(Math.random() * shapeArray.length);
+        startWithCoordinate(shapeArray[shapeIndex][0], y, x,"background-color:red");
+        shapeState = 0;
     }
 }
 
@@ -203,7 +219,7 @@ function bringBlocksToLeftByOne() {
         if (rows[i].children[0].style.backgroundColor == "red")return false;
     }
 
-    for (var i = rows.length - 1; i >= 0; i--) {
+    for (i = rows.length - 1; i >= 0; i--) {
         for (j = 0; j < rows[i].childElementCount - 1; j++) {
             if (i > 0) {
                 if (rows[i].children[j].style.backgroundColor == "red" && j == 0)return false;
@@ -224,7 +240,7 @@ function bringBlocksToRightByOne() {
         if (rows[i].children[9].style.backgroundColor == "red")return false;
     }
 
-    for (var i = rows.length - 1; i >= 0; i--) {
+    for (i = rows.length - 1; i >= 0; i--) {
         for (j = rows[i].childElementCount - 1; j > 0; j--) {
             if (i > 0) {
                 if (rows[i].children[j].style.backgroundColor == "red" && j == 9)return false;
@@ -237,27 +253,17 @@ function bringBlocksToRightByOne() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    //your code to run since DOM is loaded and ready
-    shapeColoring(shapetype[Math.floor(Math.random() * 6) + 0]);
-
-});
-
 document.addEventListener('keydown', function (event) {
     if (event.keyCode == 37) {
+        x -= 1;
         bringBlocksToLeftByOne();
     }
 });
 
-function rotateShape() {
-    //state=(state+1)%4;
-
-
-}
-
 
 document.addEventListener('keydown', function (event) {
     if (event.keyCode == 39) {
+        x += 1;
         bringBlocksToRightByOne();
     }
 });
@@ -278,17 +284,39 @@ document.addEventListener('keyup', function (event) {
     }
 });
 
-document.addEventListener('keyup', function (event) {
-    if (event.keyCode == 38) {
-        rotateShape();
+var flag=true;
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 38&&flag) {
+        flag=false;
+        shapeState = (shapeState + 1) % shapeArray[shapeIndex].length;
+
+
+        var rows = document.getElementsByClassName("row");
+        console.log('aa');
+        for (i = rows.length - 1; i >= 0; i--) {
+
+            for (j = rows[i].childElementCount - 1; j > 0; j--) {
+                if (i == x && j == y) {
+                    for(var k=0;k<5;k++){
+                        for(var l = 0;l<5;l++){
+                            if(rows[y+l].children[x+k].style.backgroundColor=="red"){
+                                rows[y+l].children[x+k].style.backgroundColor="black";
+                                //startWithCoordinate(shapeArray[0][0], y, x,"background-color:red");
+                            }
+                        }
+
+                    }
+                    console.log(shapeArray[shapeIndex][shapeState]);
+                }
+            }
+        }
+
     }
 });
 
 function callback() {
     bringBlocksDownByOne();
     setTimeout(callback, speed);
-
-
 }
 
 setTimeout(callback, speed);
